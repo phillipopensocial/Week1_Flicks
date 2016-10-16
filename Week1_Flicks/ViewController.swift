b3cb788cbb1d21b2f722cde48b36fa6d2861d175
phillipopensocial <phillip.opensocial@gmail.com>
2016-10-15 23:15:08 -0700

    Initial Commit

---
+++ Week1_Flicks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week1_Flicks
+//
+//  Created by Phillip Pang on 10/15/16.
+//  Copyright © 2016 Phillip Pang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
